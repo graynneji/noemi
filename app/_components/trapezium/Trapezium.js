cd95b791cf912ff6a9dd8173361c5750c8a813c6
Ukaegbu Gray Nneji <graynneji@outlook.com>
2024-08-03 03:22:53 -0700

    trapezium

--- app/_components/trapezium/Trapezium.js
+++ app/_components/trapezium/Trapezium.js
@@ -1,10 +1,8 @@
 import styles from "./trapezium.module.css";
 export default function Trapezium() {
   return (
-    <>
-      <div className={styles.trapezium}>
-        <h3 className={styles.hThree}>Explore our different service options</h3>
-      </div>
-    </>
+    <div className={styles.trapezium}>
+      <h3 className={styles.hThree}>Explore our different service options</h3>
+    </div>
   );
 }
